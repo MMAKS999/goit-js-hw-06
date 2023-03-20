9fc895dda442b478b91d275aef54cf7a83dee6f3
MAX999 <max999b@gmail.com>
2023-03-20 18:04:03 +0200

    Update task-02.js
    2ге завдання

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,35 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+console.log(ingredientsEl);
+
+
+// const fragment = document.createDocumentFragment();
+
+// ingredients.forEach((element) => {
+//   const lastItem = document.createElement('li');
+//   lastItem.textContent = element;
+//   lastItem.classList.add('item');
+//   fragment.appendChild(lastItem);
+
+// });
+
+// ingredientsEl.appendChild(fragment);
+// console.log(ingredientsEl);
+
+// Як краще, чи краще так ? 
+const items = ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  return item;
+  
+});
+console.log (items)
+
+// Додавання пустого рядка очищає 
+ingredientsEl.innerHTML = '';
+ingredientsEl.append(...items);
+console.log(ingredientsEl);
